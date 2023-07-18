d275df07491f08b49b029694ddb98dee16a811ed
Roshan Acharya <roshanacharya207@gmail.com>
2023-07-18 20:07:24 +0545

    routing

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>About
+        <p> This is about page</p> 
+           </div>
+
+  )
+}
+
+export default About
---
+++ src/pages/homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div>homepage
+        <p>this is homepage</p>
+    </div>
+  )
+}
+
+export default Homepage
